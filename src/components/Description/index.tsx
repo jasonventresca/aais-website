11d07bb00a8f63b634043a2b925baaaed3cc275c
Jason Ventresca <jasonventresca@gmail.com>
2025-01-17 22:01:43 -0600

    added the blue background alternating across sections. not sure i like it, though.

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -2,191 +2,211 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <section
-      id="about"
-      className="bg-gray-1 pb-8 pt-20 dark:bg-dark-2 lg:pb-[70px] lg:pt-[120px]"
-    >
-      <div className="container">
-        <div className="wow fadeInUp" data-wow-delay=".2s">
-          <div className="-mx-4 flex flex-wrap items-center">
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="mb-12 max-w-[540px] lg:mb-0">
-                <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-                  AI Solutions for Real Businesses
-                </h2>
-                <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
-                  At Alpine AI Solutions, we specialize in bringing the power of artificial intelligence to small and medium-sized businesses in Northern Arizona.
-                  <br /> <br />
-                  We understand that as a business owner, your priority is growth, efficiency, and staying competitive—not deciphering complex technology. That’s where we come in.
-                </p>
+    <>
+      <section
+        id="about"
+        className="bg-gray-1 pb-8 pt-20 dark:bg-dark-2 lg:pb-[70px] lg:pt-[120px]"
+      >
+        <div className="container">
+          <div className="wow fadeInUp" data-wow-delay=".2s">
+            <div className="-mx-4 flex flex-wrap items-center">
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="mb-12 max-w-[540px] lg:mb-0">
+                  <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
+                    AI Solutions for Real Businesses
+                  </h2>
+                  <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
+                    At Alpine AI Solutions, we specialize in bringing the power of artificial intelligence to small and medium-sized businesses in Northern Arizona.
+                    <br /> <br />
+                    We understand that as a business owner, your priority is growth, efficiency, and staying competitive—not deciphering complex technology. That’s where we come in.
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
-                <div className="w-full p-[50px] sm:w-full">
-                  <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
-                    <Image
-                      src="/images/about/about-image-02.jpg"
-                      alt="about image"
-                      fill
-                      className="h-full w-full object-cover object-center"
-                    />
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
+                  <div className="w-full p-[50px] sm:w-full">
+                    <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
+                      <Image
+                        src="/images/about/about-image-02.jpg"
+                        alt="about image"
+                        fill
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="about"
+        className="bg-primary pb-8 pt-20 dark:bg-dark-2 lg:pb-[70px] lg:pt-[120px]"
+      >
+        <div className="container">
+          <div className="wow fadeInUp" data-wow-delay=".2s">
+            <div className="-mx-4 flex flex-wrap items-center lg:flex-row-reverse">
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="mb-12 max-w-[540px] lg:mb-0">
+                  <h2 className="mb-5 text-white text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
+                    Custom AI That Delivers Results
+                  </h2>
+                  <p className="mb-10 text-white text-base leading-relaxed text-body-color dark:text-dark-6">
+                  We offer hands-on, face-to-face service to design and implement custom AI solutions tailored to your unique business needs.
+                    <br /> <br />
+                    Whether it’s fine-tuning AI models to match your brand’s voice, automating workflows with custom-built tools, or setting up data analysis systems that turn your spreadsheets into actionable insights, our solutions are designed to increase profits, lower operating costs, and save you time.
+                  </p>
+
+                </div>
+              </div>
+
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
+                  <div className="w-full p-[50px] sm:w-full">
+                    <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
+                      <Image
+                        src="/images/about/about-image-02.jpg"
+                        alt="about image"
+                        fill
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="about"
+        className="bg-gray-1 pb-8 pt-20 dark:bg-dark-2 lg:pb-[70px] lg:pt-[120px]"
+      >
+        <div className="container">
+          <div className="wow fadeInUp" data-wow-delay=".2s">
+            <div className="-mx-4 flex flex-wrap items-center">
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="mb-12 max-w-[540px] lg:mb-0">
+                  <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
+                    Personal Service, Practical Results
+                  </h2>
+                  <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
+                    Our approach is rooted in personal connection. With a focus on listening and understanding your unique needs, we ensure you feel empowered and in control every step of the way. 
+                    <br /> <br />
+                    We take the time to understand your challenges and goals, building solutions that are practical, intuitive, and seamlessly integrated into your existing processes.
+                    <br /> <br />
+                    From boutique hotels to retail stores, property management firms, and more, our clients trust us to deliver measurable results that boost their bottom line while freeing up valuable time for what matters most.
+                  </p>
+
+                </div>
+              </div>
+
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
+                  <div className="w-full p-[50px] sm:w-full">
+                    <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
+                      <Image
+                        src="/images/about/about-image-02.jpg"
+                        alt="about image"
+                        fill
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className="container">
-        <div className="wow fadeInUp" data-wow-delay=".2s">
-          <div className="-mx-4 flex flex-wrap items-center lg:flex-row-reverse">
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="mb-12 max-w-[540px] lg:mb-0">
-                <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-                  Custom AI That Delivers Results
-                </h2>
-                <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
-                We offer hands-on, face-to-face service to design and implement custom AI solutions tailored to your unique business needs.
-                  <br /> <br />
-                  Whether it’s fine-tuning AI models to match your brand’s voice, automating workflows with custom-built tools, or setting up data analysis systems that turn your spreadsheets into actionable insights, our solutions are designed to increase profits, lower operating costs, and save you time.
-                </p>
+      <section
+        id="about"
+        className="bg-primary pb-8 pt-20 dark:bg-dark-2 lg:pb-[70px] lg:pt-[120px]"
+      >
+        <div className="container">
+          <div className="wow fadeInUp" data-wow-delay=".2s">
+            <div className="-mx-4 flex flex-wrap items-center lg:flex-row-reverse">
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="mb-12 max-w-[540px] lg:mb-0">
+                  <h2 className="mb-5 text-white text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
+                    Empowering You with AI
+                  </h2>
+                  <p className="mb-10 text-white text-base leading-relaxed text-body-color dark:text-dark-6">
+                    What sets Alpine AI Solutions apart is our commitment to clarity and partnership. We demystify AI, making it accessible and actionable for businesses like yours.
+                    <br /> <br />
+                    With integrity, respect, and a passion for quality craftsmanship, we’re here to help you harness technology to work smarter, not harder, driving both efficiency and profitability.
+                  </p>
 
+                </div>
               </div>
-            </div>
 
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
-                <div className="w-full p-[50px] sm:w-full">
-                  <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
-                    <Image
-                      src="/images/about/about-image-02.jpg"
-                      alt="about image"
-                      fill
-                      className="h-full w-full object-cover object-center"
-                    />
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
+                  <div className="w-full p-[50px] sm:w-full">
+                    <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
+                      <Image
+                        src="/images/about/about-image-02.jpg"
+                        alt="about image"
+                        fill
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-
           </div>
         </div>
-      </div>
+      </section>
 
-      <div className="container">
-        <div className="wow fadeInUp" data-wow-delay=".2s">
-          <div className="-mx-4 flex flex-wrap items-center">
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="mb-12 max-w-[540px] lg:mb-0">
-                <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-                  Personal Service, Practical Results
-                </h2>
-                <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
-                  Our approach is rooted in personal connection. With a focus on listening and understanding your unique needs, we ensure you feel empowered and in control every step of the way. 
-                  <br /> <br />
-                  We take the time to understand your challenges and goals, building solutions that are practical, intuitive, and seamlessly integrated into your existing processes.
-                  <br /> <br />
-                  From boutique hotels to retail stores, property management firms, and more, our clients trust us to deliver measurable results that boost their bottom line while freeing up valuable time for what matters most.
-                </p>
+      <section
+        id="about"
+        className="bg-gray-1 pb-8 pt-20 dark:bg-dark-2 lg:pb-[70px] lg:pt-[120px]"
+      >
+        <div className="container">
+          <div className="wow fadeInUp" data-wow-delay=".2s">
+            <div className="-mx-4 flex flex-wrap items-center">
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="mb-12 max-w-[540px] lg:mb-0">
+                  <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
+                    Your Business, Elevated
+                  </h2>
+                  <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
+                    Take the next step with Alpine AI Solutions—because your business deserves more than one-size-fits-all technology.
+                    <br /> <br />
+                    Together, we’ll create tools that accelerate your success, reduce operating costs, and save time while keeping the human touch at the heart of everything we do.
+                  </p>
 
+                </div>
               </div>
-            </div>
 
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
-                <div className="w-full p-[50px] sm:w-full">
-                  <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
-                    <Image
-                      src="/images/about/about-image-02.jpg"
-                      alt="about image"
-                      fill
-                      className="h-full w-full object-cover object-center"
-                    />
+              <div className="w-full px-4 lg:w-1/2">
+                <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
+                  <div className="w-full p-[50px] sm:w-full">
+                    <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
+                      <Image
+                        src="/images/about/about-image-02.jpg"
+                        alt="about image"
+                        fill
+                        className="h-full w-full object-cover object-center"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
 
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="container">
-        <div className="wow fadeInUp" data-wow-delay=".2s">
-          <div className="-mx-4 flex flex-wrap items-center lg:flex-row-reverse">
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="mb-12 max-w-[540px] lg:mb-0">
-                <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-                  Empowering You with AI
-                </h2>
-                <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
-                  What sets Alpine AI Solutions apart is our commitment to clarity and partnership. We demystify AI, making it accessible and actionable for businesses like yours.
-                  <br /> <br />
-                  With integrity, respect, and a passion for quality craftsmanship, we’re here to help you harness technology to work smarter, not harder, driving both efficiency and profitability.
-                </p>
-
-              </div>
-            </div>
-
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
-                <div className="w-full p-[50px] sm:w-full">
-                  <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
-                    <Image
-                      src="/images/about/about-image-02.jpg"
-                      alt="about image"
-                      fill
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="wow fadeInUp" data-wow-delay=".2s">
-          <div className="-mx-4 flex flex-wrap items-center">
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="mb-12 max-w-[540px] lg:mb-0">
-                <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-                  Your Business, Elevated
-                </h2>
-                <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
-                  Take the next step with Alpine AI Solutions—because your business deserves more than one-size-fits-all technology.
-                  <br /> <br />
-                  Together, we’ll create tools that accelerate your success, reduce operating costs, and save time while keeping the human touch at the heart of everything we do.
-                </p>
-
-              </div>
-            </div>
-
-            <div className="w-full px-4 lg:w-1/2">
-              <div className="-mx-2 flex flex-wrap sm:-mx-4 lg:-mx-2 xl:-mx-4">
-                <div className="w-full p-[50px] sm:w-full">
-                  <div className="relative mb-4 sm:mb-8 sm:h-[220px] md:h-[346px] lg:mb-4 lg:h-[225px] xl:mb-8 xl:h-[310px]">
-                    <Image
-                      src="/images/about/about-image-02.jpg"
-                      alt="about image"
-                      fill
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
