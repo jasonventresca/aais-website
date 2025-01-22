abe04d0e71d90d0c85c67b33ad5a5459068bcd9d
Jason Ventresca <jasonventresca@gmail.com>
2025-01-22 15:26:49 -0600

    Revert "Setting up GitHub Pages deployment."
    This reverts commit 614ebf3ac7c8e81cd65149f35990e66e46760d73.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: isProd ? '/aais-website/' : '',
-  basePath: isProd ? '/aais-website' : '',
-  output: 'export'
-};
+const nextConfig = {}
 
-//export default nextConfig;
-module.exports = nextConfig;
+module.exports = nextConfig
